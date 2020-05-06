7e8446ca57d435e58239dd012534a9bdbb740e5d
Dmitry Antonov <pixwork@yandex.ru>
2020-05-06 18:57:51 +0300

    Fixed Dicom amplitude

--- converter/dicom.py
+++ converter/dicom.py
@@ -4,6 +4,8 @@ import pydicom
 import struct
 import io
 
+MILLIVOLTS_TO_MICROVOLTS = 1000.0
+
 dataset = pydicom.dcmread(io.BytesIO(sys.stdin.buffer.read()))
 
 dicom = dataset
@@ -13,15 +15,15 @@ sequence_item = dicom.WaveformSequence[0]
 assert (sequence_item.WaveformSampleInterpretation == 'SS')
 
 channel_definitions = sequence_item.ChannelDefinitionSequence
-wavewform_data = sequence_item.WaveformData
+waveform_data = sequence_item.WaveformData
 channels_no = sequence_item.NumberOfWaveformChannels
 samples = sequence_item.NumberOfWaveformSamples
 
 assert channels_no == 12
 
-unpack_fmt = '<%dh' % (len(wavewform_data) / 2)
-unpacked_waveform_data = struct.unpack(unpack_fmt, wavewform_data)
-signals = np.asarray(unpacked_waveform_data, dtype=np.float64).reshape(
-    samples, channels_no).transpose()[:, ::2]
+unpack_fmt = '<%dh' % (len(waveform_data) / 2)
+unpacked_waveform_data = struct.unpack(unpack_fmt, waveform_data)
+signals = (np.asarray(unpacked_waveform_data, dtype=np.float64).reshape(
+    samples, channels_no).transpose()[:, ::2] / MILLIVOLTS_TO_MICROVOLTS)
 
 sys.stdout.buffer.write(signals.tobytes())
